d8934a166b7318206d558005e8e8ba22ea2a56fa
Eran Krakovsky <ekrako@gmail.com>
2019-01-26 18:45:20 +0200

    Initial commit

---
+++ app/src/main/java/info/krakovsky/menudemo/MainActivity.java
@@ -0,0 +1,41 @@
+package info.krakovsky.menudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater =  getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                Toast.makeText(this, "menu Settings was clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_help:
+                Toast.makeText(this, "menu help was clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
